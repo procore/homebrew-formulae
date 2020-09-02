0db98606c145e870c9f42f442338d477a77723e3
Maxine Krebs <maxine.krebs@procore.com>
2020-09-02 15:27:27 -0700

    Brew formula update for nox version v0.1.4

--- Formula/nox.rb
+++ Formula/nox.rb
@@ -2,9 +2,18 @@
 class Nox < Formula
   desc "A grand unified Elasticsearch infrastructure management cli"
   homepage ""
-  url "https://github.com/procore/nox/releases/download/v0.1.3/nox_0.1.3_64-bit_macOS.tar.gz"
-  version "0.1.3"
-  sha256 "6581c32d00085f03263040a7aec75bbfa613e55063eb4ce3d3e6799e770dcff1"
+  version "0.1.4"
+  bottle :unneeded
+
+  if OS.mac?
+    url "https://github.com/procore/nox/releases/download/v0.1.4/nox_0.1.4_64-bit_macOS.tar.gz"
+    sha256 "3ee8bfb8a9f9ffe83de7c682eada35c0105bffa0304b43172915e045abf214c2"
+  elsif OS.linux?
+    if Hardware::CPU.intel?
+      url "https://github.com/procore/nox/releases/download/v0.1.4/nox_0.1.4_64-bit_linux.tar.gz"
+      sha256 "f7156ff9873534f0e31ef1e0bf8ee6cb15f5b7d98f764e221b47cd64df794277"
+    end
+  end
 
   def install
     bin.install "nox"
